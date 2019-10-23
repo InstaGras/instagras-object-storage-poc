5ffbd96a529edf4ef11b126229e5c0eac1b2f6be
Alban GUILLET <albanguillet98@gmail.com>
2019-10-23 11:43:08 +0200

    adding elements

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+extern crate dotenv;
+extern crate rusoto_connect;
+extern crate rusoto_core;
+extern crate rusoto_s3;
+extern crate rusoto_sqs;
+
+use std::env;
+use dotenv::dotenv;
+use rusoto_connect::Credentials;
+use rusoto_s3::S3Client;
+
 fn main() {
-    println!("Hello, world!");
+
+    dotenv().expect("Failed to read .env file.");
+    println!("{}", env::var("S3_ACCESS_KEY").expect("S3_ACCESS_KEY not found."));
+
+//    let client = new S3Client;
+
+    let credentials = Credentials {
+        access_token: env::var("S3_ACCESS_KEY"),
+    };
+
+    // S3Client.get_object(input: GetObjectRequest)
+    
+    // S3Client.put_object(input: PutObjectRequest)
 }
